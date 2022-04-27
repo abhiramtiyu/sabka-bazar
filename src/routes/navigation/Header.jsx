492d3b1698d0155bf0863eeb30ac4fb3673d84c7
Abhiram Tiyu <abhiram.tiyu@publicissapient.com>
2022-04-27 17:29:44 +0530

    removed dummy data

--- src/routes/navigation/Header.jsx
+++ src/routes/navigation/Header.jsx
@@ -20,28 +20,28 @@ const Header=()=>{
     return(
       <>
         <header>
-       
        <Navbar collapseOnSelect expand="lg" className='shadow-sm headerindeex'>
        <Container>
-         <Link to="/" className='navbar-brand'><img src="/images/logo_2x.png" className="brandlogoimg" alt="Sabka Bazar Brand Logo" />
+         <Link to="/" className='navbar-brand'>
+           <img src="/static/images/logo_2x.png" className="brandlogoimg" alt="Sabka Bazar Brand Logo" />
          </Link>
-       <div id="responsive-navbar-nav" className='top-bar-row navbar-collapse collapse'>
-       <div className='topbar'>
-         <Nav className="me-auto justify-content-end">
-             <Link to="/login" className='nav-link'>SignIn</Link>
-             <Link to="/register" className='nav-link'>Register</Link>
-         </Nav>
-       </div>
-         <Nav className="me-auto">
-             <Link to="/" className='nav-link'>Home</Link>
-             <Link to="/pplpage" className='nav-link'>Products</Link>
-         </Nav>
-         <div className='cartsection'>
-         <Button className='btn cart-btn btn-default' variant="light" onClick={cartShow}><i className="bi bi-cart"></i> {context.cart.length} Item</Button>
-         
-         {cartPopup ? <CartPopup cartClose={cartShow} cartData={context.cart}/> : null }
-         </div>
-       </div>
+        <div id="responsive-navbar-nav" className='top-bar-row navbar-collapse collapse'>
+          <div className='topbar'>
+            <Nav className="me-auto justify-content-end">
+                <Link to="/login" className='nav-link'>SignIn</Link>
+                <Link to="/register" className='nav-link'>Register</Link>
+            </Nav>
+          </div>
+          <Nav className="me-auto">
+              <Link to="/" className='nav-link'>Home</Link>
+              <Link to="/pplpage" className='nav-link'>Products</Link>
+          </Nav>
+          <div className='cartsection'>
+          <Button className='btn cart-btn btn-default' variant="light" onClick={cartShow}><i className="bi bi-cart"></i> {context.cart.length} Item</Button>
+          
+          {cartPopup ? <CartPopup cartClose={cartShow} cartData={context.cart}/> : null }
+          </div>
+        </div>
        </Container>
      </Navbar>
       </header>
